0c3be2bb95db725f4d6aa1caa5ce958c96cf7dc4
poornimam86 <poornimameduri@kluniversity.in>
2026-08-03 21:06:19 +0530

    Create Main.java

---
+++ Labs/Lab5/Main.java
@@ -0,0 +1,81 @@
+package com.portfolio.app;
+import java.util.Scanner;
+import com.portfolio.service.PortfolioService;
+import java.util.Scanner;
+
+import com.portfolio.service.PortfolioService;
+import com.portfolio.threading.PriceFeedSimulator;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        PortfolioService service = new PortfolioService();
+
+        int choice;
+        Thread priceThread =
+                new Thread(new PriceFeedSimulator(service.getStocks()));
+
+        priceThread.setDaemon(true);
+        priceThread.start();
+        
+        do {
+        	System.out.println("\n===== Portfolio Management System =====");
+        	System.out.println("1. Add Stock");
+        	System.out.println("2. Display Stocks");
+        	System.out.println("3. Search Stock");
+        	System.out.println("4. Update Stock");
+        	System.out.println("5. Delete Stock");
+        	System.out.println("6. Display Portfolio Value");
+        	System.out.println("7. Benchmark Lookup");
+        	System.out.println("8. Exit");
+        	System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+
+                case 1:
+                    service.addStock();
+                    break;
+
+                case 2:
+                    service.displayStocks();
+                    break;
+                    
+                case 3:
+                    service.searchStock();
+                    break;
+
+                case 4:
+                    service.updateStock();
+                    break;
+
+                case 5:
+                    service.deleteStock();
+                    break;
+                case 6:
+                    service.showPortfolioValue();
+                    break;
+                case 7:
+                    service.benchmarkLookup();
+                    break;
+                case 8:
+                	priceThread.interrupt();
+                    System.out.println("Stopping Price Feed...");
+                    System.out.println("Thank you for using Portfolio Management System.");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+        } while (choice != 8);
+        try {
+            priceThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        scanner.close();
+    }
+}
