be01e79d00cfb9c76e5f4733d2e07a4e411ea671
poornimam86 <poornimameduri@kluniversity.in>
2026-08-04 15:49:44 +0530

    Create InsufficientHoldingException.java

---
+++ Labs/Lab9/InsufficientHoldingException.java
@@ -0,0 +1,9 @@
+package com.portfolio.exception;
+
+public class InsufficientHoldingException extends Exception {
+
+    public InsufficientHoldingException(String message) {
+        super(message);
+    }
+
+}
